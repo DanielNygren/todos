966eac4ff5b90115b4174d72b239d8d2b0af3920
Daniel <75730040+DanielNygren@users.noreply.github.com>
2021-10-13 20:53:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,216 +1,220 @@
+const form = document.querySelector('form');
+const todoList = document.querySelector('#todo-list')
+const checkAllButton = document.querySelector('#check-all');
+const noteContainer = document.querySelector('#note-container');
+const inputContainer = document.querySelector('#input-container');
+const clearCompletedButton = document.querySelector('#clearCompletedButton');
+const localStorageList = [];
+const filter = document.querySelectorAll('.filters li');
+const itemsLeftLabel = document.querySelector('#counter');
+const sectionMain = document.querySelector('.main');
+const sectionFooter = document.querySelector('.footer');
 
-const form = document.querySelector("form");
-const todoList = document.querySelector("#todo-list");
-const checkAllButton = document.querySelector("#check-all");
-const sectionMain = document.querySelector(".main");
-const sectionFooter = document.querySelector(".footer");
-const cearCompleted = document.querySelector(".clear-completed");
-const filter = document.querySelectorAll(".filters li");
-const main = document.querySelector(".main");
-const localStoregeList = [];
-
-//Load From Local Storege
-loadFromLocalStorege();
-
-
-
+//Load from Local Storage
+loadFromLocalStorage();
 
 form.onsubmit = event => {
     event.preventDefault();
 
-    if(form.input.value != ""){
+    if (form.input.value != "") {
         createListItem();
     }
 
     form.input.value = "";
-    saveToLocalStorege();
+    saveToLocalStorage();
+
 }
 
-function createListItem(text, checked){
+//Create a 'li' for each added task and add to the HTML ul
+function createListItem(text, checked) {
 
-    const todoItem = document.createElement("li");
-    todoItem.onmouseenter = event =>{
-        itemDeleteButten.hidden = false;
+    const todoItem = document.createElement('li');
+    todoItem.onmouseenter = event => {
+        itemDeleteButton.hidden = false;
     }
-    todoItem.onmouseleave = event =>{
-        itemDeleteButten.hidden = true;
+    todoItem.onmouseleave = event => {
+        itemDeleteButton.hidden = true;
     }
+    
     //Checkbox
-    const itemCheckbox = document.createElement("input");
-    itemCheckbox.className = "checkbox-round"
+    const itemCheckbox = document.createElement('input');;
     itemCheckbox.type = "checkbox";
-    try{
-        if(checked == "checked"){
+    itemCheckbox.className = "checkbox-round";
+
+    try {
+        if (checked == "checked") {
             itemCheckbox.checked = true;
             todoItem.className = "checked";
         }
     }
-    catch{}
-    
-    //Text label
-    const itemLabel = document.createElement("label")
-    if(text){
+    catch { }
+
+    //Text input
+    const itemLabel = document.createElement('label');
+
+    if (text) {
         itemLabel.textContent = text;
     }
-    else{
-        itemLabel.textContent = form.elements.input.value;
+    else {
+        itemLabel.textContent = form.input.value;
     }
-    
-    //Edid Text Input 
-    const itemLabelEdit = document.createElement("input");
-    itemLabelEdit.className = "edit";
-    itemLabelEdit.type = "text";
-    itemLabelEdit.hidden = "hidden";
+
+    //Edit text input
+    const itemLabelEdit = document.createElement('input');
+    itemLabelEdit.className = 'edit';
+    itemLabelEdit.type = 'text';
+    itemLabelEdit.hidden = true;
+
+    //Button
+    const itemDeleteButton = document.createElement('button');
+    itemDeleteButton.className = 'item-delete-button';
+    itemDeleteButton.textContent = '❌';
+    itemDeleteButton.hidden = true;
+
+    //Events
+    itemCheckbox.onchange = event => {
+        if (itemCheckbox.checked == true) {
+            todoItem.className = 'checked';
+        }
+        else {
+            todoItem.className = '';
+        }
+        saveToLocalStorage();
+    }
+
+    itemDeleteButton.onclick = event => {
+        todoItem.remove();
+        deleteItemInList(todoItem);
+        saveToLocalStorage();
+    }
+
+    itemLabel.ondblclick = event => {
+        itemLabel.hidden = true;
+        itemLabelEdit.hidden = false;
+        itemLabelEdit.value = itemLabel.textContent;
+        itemLabelEdit.focus();
+    }
 
     itemLabelEdit.addEventListener('focusout', (event) => {
         itemLabel.textContent = itemLabelEdit.value;
         itemLabelEdit.hidden = true;
         itemLabel.hidden = false;
-        saveToLocalStorege();
-      });
-    
-    //Item Delete Butten
-    const itemDeleteButten = document.createElement("button");
-    itemDeleteButten.className = "item-delete-button";
-    itemDeleteButten.type = "button"
-    itemDeleteButten.textContent = "❌";
-    itemDeleteButten.hidden = true;
-    //Events
-    
-    //Item Checkbox
-    itemCheckbox.onchange = event => {
-        if(itemCheckbox.checked == true){
-            todoItem.className = "checked";
-        }
-        else{
-            todoItem.className = "";
-        }
-        saveToLocalStorege();
-    }
-    //Edit Label Text
-    itemLabel.ondblclick = event => {
-        itemLabel.hidden = true;
-        itemLabelEdit.hidden = false;
-        itemLabelEdit.value = itemLabel.textContent;
-        itemLabelEdit.focus(itemLabelEdit)
-    }
+        saveToLocalStorage();
+    });
 
-    //Edit Label Event
     itemLabelEdit.onkeydown = event => {
-        if(event.key == "Enter"){
+        if (event.key == 'Enter') {
             itemLabel.textContent = itemLabelEdit.value;
             itemLabelEdit.hidden = true;
             itemLabel.hidden = false;
-            saveToLocalStorege();
+            saveToLocalStorage();
         }
     }
 
-    //Delete Button
-    itemDeleteButten.onclick = event => {
-        todoItem.remove();
-        deleteItemInList(todoItem);
-        saveToLocalStorege();
-    }
-
-    //Append Chilldren
     todoItem.appendChild(itemCheckbox);
     todoItem.appendChild(itemLabel);
     todoItem.appendChild(itemLabelEdit);
-    todoItem.appendChild(itemDeleteButten);
+    todoItem.appendChild(itemDeleteButton);
     todoList.appendChild(todoItem);
 
-    localStoregeList.push(todoItem);
+    localStorageList.push(todoItem);
+
 }
 
-//Check all todos
+//Check / Uncheck all list items
 checkAllButton.onclick = event => {
+
     let items = todoList.getElementsByTagName("li");
-    let itemsChecked = []
+    let itemsChecked = [];
+
     for (let i = 0; i < items.length; i++) {
 
-        if(items[i].className == "checked"){
+        if (items[i].className == "checked") {
 
             itemsChecked.push(items[i])
         }
     }
-    if(itemsChecked.length == items.length){
+    if (itemsChecked.length == items.length) {
+
         for (let i = 0; i < items.length; i++) {
 
             let cb = items[i].firstElementChild;
             cb.click();
         }
     }
-    else{
+    else {
         for (let i = 0; i < items.length; i++) {
 
-            if(items[i].className != "checked"){
+            if (items[i].className != "checked") {
                 let cb = items[i].firstElementChild;
                 cb.click();
             }
-        }     
+        }
     }
+
 }
 
-//Delete From Local Storege List
-function deleteItemInList(todoItem){
-    const index = localStoregeList.indexOf(todoItem);
-    localStoregeList.splice(index,1);
+//Delete from Local Storage List
+function deleteItemInList(todoItem) {
+    const index = localStorageList.indexOf(todoItem);
+    localStorageList.splice(index, 1);
+
 }
 
-//Save To Local Storege
-function saveToLocalStorege(){
+//Save to Local Storage List
+function saveToLocalStorage() {
     localStorage.clear();
     let string = "";
-    for(let i = 0; i < localStoregeList.length; i++){
-        string = localStoregeList[i].textContent + localStoregeList[i].className;
-        localStorage.setItem(i,string)
+    for (let i = 0; i < localStorageList.length; i++) {
+
+        string = localStorageList[i].textContent + localStorageList[i].className;
+        localStorage.setItem(i, string);
     }
 
     itemsLeftCounter();
     hiddenSections();
 }
 
-//Load Form Local Storege
-function loadFromLocalStorege(){
+//Load from Local Storage List
+function loadFromLocalStorage() {
     for (let i = 0; i < localStorage.length; i++) {
+
         let item = localStorage.getItem(i);
-        let value = item.split("❌");
+        let value = item.split('❌');
         createListItem(value[0], value[1]);
     }
 }
 
-//Hash Event Listener
-window.addEventListener("hashchange", hashCanged, false);
-//Run 
-hashCanged();
+//Update URL
+window.addEventListener('hashchange', hashChanged, false);
+hashChanged();
 
-//Hash Canged Check Function
-function hashCanged(){
+function hashChanged() {
     if(location.hash === "#/"){
         hiddenItem("grid", "checked")
         hiddenItem("grid", "")
         filter[0].className = "active"
-        filter[1].className = "disable";
-        filter[2].className = "disable";
+        filter[1].className = "disabled";
+        filter[2].className = "disabled";
     }
     if(location.hash === "#/active"){
         hiddenItem("none", "checked");
         hiddenItem("grid", "")
-        filter[0].className = "disable"
+        filter[0].className = "disabled"
         filter[1].className = "active"
-        filter[2].className = "disable"
+        filter[2].className = "disabled"
 
     }
     if(location.hash === "#/completed"){
         hiddenItem("none", "")
         hiddenItem("grid", "checked")
-        filter[0].className = "disable"
-        filter[1].className = "disable"
+        filter[0].className = "disabled"
+        filter[1].className = "disabled"
         filter[2].className = "active"
     }
 }
 
-//Hash Canged Viseble Result Function
+//Show / Hide listitems as Hash changes
 function hiddenItem(h, state){
     const todoListItem = document.querySelectorAll("#todo-list li")
     for(let i = 0; i < todoListItem.length; i++){
@@ -220,52 +224,56 @@ function hiddenItem(h, state){
     }
 }
 
-//Cear Comleted
-cearCompleted.onclick = event => {
-    const todoListItem = document.querySelectorAll("#todo-list li")
-    for (let i = todoListItem.length; i >= 0; i--) {
-        try{
-            if(todoListItem[i].className === "checked"){
-                localStoregeList.splice(i,1);
-                todoList.removeChild(todoListItem[i]);
+//Clear all checked items when "clear completed button" is clicked
+clearCompletedButton.onclick = event => {
+
+    const items = todoList.getElementsByTagName("li");
+    for (let i = items.length; i >= 0; i--) {
+        try {
+            if (items[i].className == "checked") {
+                localStorageList.splice(i, 1);
+                todoList.removeChild(items[i]);
             }
         }
-        catch{}    
-    }
-    saveToLocalStorege();
-}
-
-
-
-function itemsLeftCounter(){
-    const todoListItem = document.querySelectorAll("#todo-list li")
-    let itemsChecked = 0;
-    const itemsLeft = document.querySelector("#counter")
-    for (let i = 0; i < todoListItem.length; i++) {
-        if(todoListItem[i].className != "checked"){
-            itemsChecked++
+        catch {
         }
     }
-    itemsLeft.textContent = itemsChecked + " items left"
+    clearCompletedButton.hidden = true;
+    saveToLocalStorage();
+}
 
-    if(itemsChecked == 0){
-        checkAllButton.style.opacity = "1"
-    }
-    else{
-        checkAllButton.style.opacity = "0.2"
-    }
-    if((todoListItem.length - itemsChecked)  > 0){
-        cearCompleted.hidden = false;
-    }
-    else{
-        cearCompleted.hidden = true;
-    }
+//Checks how many items are left for "items left" label, and hides/shows clearCompletedButton
+function itemsLeftCounter() {
 
+    const items = todoList.getElementsByTagName("li");
+    clearCompletedButton.hidden = true;
+    let taskNumber = 0;
+
+    for (let i = 0; i < items.length; i++) {
+
+        const cb = items[i].firstElementChild;
+
+        if (cb.checked == true) {
+            clearCompletedButton.hidden = false;
+        }
+        else {
+            taskNumber++;
+        }
+    }
+    itemsLeftLabel.textContent = taskNumber + ' items left'
+
+    if (taskNumber === 0) {
+        checkAllButton.style.opacity = '1';
+    }
+    else {
+        checkAllButton.style.opacity = '0.2';
+    }
 }
 itemsLeftCounter();
 
+//Show only the input-container and hide rest of the "Container" when there are no items in list
 function hiddenSections(){
-    if(localStoregeList.length > 0){
+    if(localStorageList.length > 0){
         sectionMain.hidden = false;
         sectionFooter.hidden= false;
         checkAllButton.style.visibility = "visible";
@@ -274,6 +282,8 @@ function hiddenSections(){
     else{
         sectionMain.hidden = true;
         sectionFooter.hidden= true;
+        checkAllButton.style.visibility = "hidden";
+
     }
 }
 hiddenSections();
